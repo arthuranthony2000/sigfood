3cd60621105c25f52ef0cb6bac3437a8e4a2bdf5
Arthur Anthony <arthuranthony2000@gmail.com>
2021-08-20 00:13:27 -0300

    Ajustando inserts 2

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -28,6 +28,15 @@ INSERT INTO Consumidor
     ('037.412.268-74','Miguel Murilo Pires', 'Masculino', 25, 'miguel.murilo'),
     ('148.506.661-16','Gustavo Caio Nunes', 'Masculino', 28, 'gustavo.caio');
 
+-- CARTÃO FIDELIDADE
+
+INSERT INTO Cartao_Fidelidade(pontuacao, cpf_consumidor)
+	VALUES
+	(30, '466.539.997-26'),
+	(10, '944.345.122-00'),
+	(40, '248.934.605-95'),
+	(50, '037.412.268-74'),
+	(40, '148.506.661-16');
 
 -- ADMINISTRADOR
 
@@ -62,6 +71,36 @@ INSERT INTO Cardapio(nome, descricao, valor, promocao, estrelas, fk_codigo_categ
     	('Prato C', 'Prato Simples 3', 52.23, FALSE, 5, 3),
     	('Prato D', 'Prato Simples 4', 53.23, FALSE, 4, 4),
     	('Prato E', 'Prato Simples 5', 55.23, FALSE, 4, 5);
+
+-- INGREDIENTE
+
+INSERT INTO Ingrediente(nome) 
+    VALUES
+    ('ingrediente01'), ('ingrediente02'), 
+    ('ingrediente03'), ('ingrediente04'), 
+    ('ingrediente05'), ('ingrediente06'), 
+    ('ingrediente07'), ('ingrediente08');
+
+-- COMBO
+
+INSERT INTO Combo(fk_codigo_1_cardapio, fk_codigo_2_cardapio)
+	VALUES
+	(1, 2),
+	(1, 3),
+	(2, 3),
+	(2, 4),
+	(4, 1);
+
+-- KEY
+
+INSERT INTO Key(valor, codigo_admin)
+	VALUES
+	('a7d3b3c8785fb0c32c222f3d7ddd67af', 1), -- PERMISSÃO 001
+	('84da5bb2d468174566581fd4a806f97d', 2), -- PERMISSÃO 002
+	('a2b453013abb1da3e1eab65a3f28c338', 3), -- PERMISSÃO 003
+	('ce053a95277219c9b3f6724c222a1565', 4), -- PERMISSÃO 004
+	('caef1a04e10a29bde33b90d6ef8bae83', 5); -- PERMISSÃO 005
+	
 
 
 
