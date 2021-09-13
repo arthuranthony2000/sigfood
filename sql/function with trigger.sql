f6f33f8739cc5425bebe5f994f94ecfebba46d41
Adalberto Maia <53623750+batbeto@users.noreply.github.com>
2021-09-13 13:51:01 -0300

    function and trigger

---
+++ sql/function with trigger.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR REPLACE FUNCTION tipo_usuario_log_func()
+RETURNS TRIGGER AS $$
+BEGIN
+	INSERT INTO Administrador (nome, tipo, fk_username_usuario)
+	VALUES (NEW.nome, NEW.TIPO, NEW.fk_username_usuario);
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tipo_usuario
+	AFTER INSERT ON Usuario
+	FOR EACH ROW
+	EXECUTE PROCEDURE tipo_usuario_log_func();
+
