4ac63b4619358de8c3157075cc40488d7eb74c2d
Adalberto Maia <53623750+batbeto@users.noreply.github.com>
2021-09-13 14:10:25 -0300

    function

---
+++ sql/function.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION check_pass(username TEXT, password TEXT)
+RETURNS BOOLEAN AS $$
+DECLARE passed BOOLEAN;
+BEGIN
+        SELECT  (pwd = $2) INTO passed
+        FROM    Usuario
+        WHERE   username = $1;
+
+        RETURN passed;
+END;
+$$  LANGUAGE plpgsql
+    SECURITY DEFINER SET search_path = administrador, pg_temp;
+
