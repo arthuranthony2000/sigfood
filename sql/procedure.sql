ebd0b370f520be9730f582d9859955c58b3e0145
Adalberto Maia <53623750+batbeto@users.noreply.github.com>
2021-09-13 14:31:05 -0300

    procedure é uma função sem retorno

---
+++ sql/procedure.sql
@@ -0,0 +1,8 @@
+﻿BEGIN;
+CREATE FUNCTION check_pass(username TEXT, password TEXT) SECURITY DEFINER;
+REVOKE ALL ON FUNCTION check_pass(uname TEXT, password TEXT) FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION check_pass(username TEXT, password TEXT) TO admins;
+COMMIT;
+
+
+-- função sem retorno (basicamente)a
