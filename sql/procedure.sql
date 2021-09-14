e34eeca74c2e65739d94bf152b08fc9c0b10ec67
Adalberto Maia <53623750+batbeto@users.noreply.github.com>
2021-09-13 22:47:01 -0300

    inserir cartão fidelidade

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,8 +1,18 @@
-﻿BEGIN;
+--Revoga direitos
+
+BEGIN;
 CREATE FUNCTION check_pass(username TEXT, password TEXT) SECURITY DEFINER;
 REVOKE ALL ON FUNCTION check_pass(uname TEXT, password TEXT) FROM PUBLIC;
 GRANT EXECUTE ON FUNCTION check_pass(username TEXT, password TEXT) TO admins;
 COMMIT;
 
 
--- função sem retorno (basicamente)a
+-- Insere cartão fidelidade
+
+CREATE OR REPLACE FUNCTION InsereCartao(pontuacao INT, cpf_consumidor TEXT)
+      RETURNS void AS $$
+      BEGIN
+        INSERT INTO Cartao_Fidelidade
+         VALUES (pontuacao, cpf_consumidor);
+      END;
+      $$ LANGUAGE 'plpgsql';
