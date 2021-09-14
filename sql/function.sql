93be8574eaa18118d3c7c5e97693d86d29d3fcb0
Adalberto Maia <53623750+batbeto@users.noreply.github.com>
2021-09-13 21:33:24 -0300

    contador update

--- sql/function.sql
+++ sql/function.sql
@@ -1,4 +1,6 @@
-﻿CREATE FUNCTION check_pass(username TEXT, password TEXT)
+--Verificador para usuario e senha!
+
+CREATE FUNCTION check_pass(username TEXT, password TEXT)
 RETURNS BOOLEAN AS $$
 DECLARE passed BOOLEAN;
 BEGIN
@@ -11,3 +13,19 @@ END;
 $$  LANGUAGE plpgsql
     SECURITY DEFINER SET search_path = administrador, pg_temp;
 
+-- Contador de usuarios por idade
+
+CREATE FUNCTION contador_usuarios(idade1 INT, idade2 INT)
+RETURNS INT
+LANGUAGE plpgsql AS $$
+DECLARE
+   contador INTEGER;
+BEGIN
+   SELECT count(*) 
+   INTO contador
+   FROM Usuarios
+   WHERE idade BETWEEN idade1 and idade2;
+   
+   RETURN contador;
+END;
+$$;
